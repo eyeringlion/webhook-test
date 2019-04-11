468a59bcebde4f17e86137c770c31be05632aaff
eyeringlion <eyeringlion@users.noreply.github.com>
2019-04-11 13:49:19 -0400

    Update index.js
    Use locations-api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 module['exports'] = function helloWorld (hook) {
-  var url = "https://locations-api.wework.com/api/v1/geogroupings/new-york-city--NY"";
+  var url = "https://locations-api.wework.com/api/v1/geogroupings/new-york-city--NY";
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = myfunction;
 
