51af09ad04bf40679f2fb55264aa7752df442e09
irene <irene.chang@wework.com>
2019-04-11 14:24:31 -0400

    CORS

--- index.js
+++ index.js
@@ -4,20 +4,24 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 module['exports'] = function helloWorld (hook) {
+  var proxy = "https://cors-anywhere.herokuapp.com/";
   var url = "https://locations-api.wework.com/api/v1/geogroupings/new-york-city--NY";
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = myfunction;
 
-  xmlhttp.open("GET", url, true);
+  // xmlhttp.open("GET", url, true);
+  xmlhttp.open("GET", proxy + url);
+  // xmlhttp.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+  xmlhttp.setRequestHeader("Access-Control-Allow-Headers", "x-requested-with, x-requested-by");
 
   xmlhttp.send(null);
-  hook.res.json({});
+  // hook.res.json({});
 
 
   function myfunction() {
     if (xmlhttp.readyState == 4) {
-      var json = JSON.parse(xmlhttp.responseText);
-      hook.res.json({test: "test"});
+      // var json = JSON.parse(xmlhttp.responseText);
+      hook.res.json({text: xmlhttp.responseText});
     }
   }
 }
