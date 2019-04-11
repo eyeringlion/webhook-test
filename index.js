2c603048f8a2b4a2a550ff0f5b9d8e59ca137594
irene <irene.chang@wework.com>
2019-04-11 15:59:16 -0400

    json

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var fetch = require('node-fetch');
 module['exports'] = function helloWorld (hook) {
   var proxyurl = "https://cors-anywhere.herokuapp.com/";
   var url = "https://locations-api.wework.com/api/v1/geogroupings/new-york-city--NY"; // site that doesn’t send Access-Control-*
-  fetch(url, {
+  fetch(proxyurl + url, {
     headers: {
       'X-Requested-With': 'Content-Type'
     }
   })
-    .then(res => res.json())
-    .then(json => {
-      hook.res.json({text: "text"})
+    .then(res => res.text())
+    .then(text => {
+      hook.res.json({text: text})
     });
 }
